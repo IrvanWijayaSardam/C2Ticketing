0165034a257ac1ecc9359255b05f7a391fc79a60
Roisull <lusiro2001@students.amikom.ac.id>
2022-11-21 17:12:17 +0700

    make navigation, package, fragment

--- app/src/main/java/com/ctwofinalproject/ticketing/view/MainActivity.kt
+++ app/src/main/java/com/ctwofinalproject/ticketing/view/MainActivity.kt
@@ -1,7 +1,8 @@
-package com.ctwofinalproject.ticketing
+package com.ctwofinalproject.ticketing.view
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.ctwofinalproject.ticketing.R
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
