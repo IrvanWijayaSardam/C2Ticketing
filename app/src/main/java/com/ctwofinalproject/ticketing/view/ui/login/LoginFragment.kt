b63e1860fa57120950f4039151feb7f292b36e2c
Roisull <lusiro2001@students.amikom.ac.id>
2022-11-27 01:16:44 +0700

    slicing signUp_fragment progress 1.7 & slicing login_fragment 1.2

--- app/src/main/java/com/ctwofinalproject/ticketing/view/ui/login/LoginFragment.kt
+++ app/src/main/java/com/ctwofinalproject/ticketing/view/ui/login/LoginFragment.kt
@@ -5,15 +5,30 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.navigation.Navigation
 import com.ctwofinalproject.ticketing.R
+import com.ctwofinalproject.ticketing.databinding.FragmentLoginBinding
 
 class LoginFragment : Fragment() {
+    private var _binding: FragmentLoginBinding?                 = null
+    private val binding get()                                   = _binding!!
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_login, container, false)
+        _binding = FragmentLoginBinding.inflate(inflater,container, false)
+        return binding?.root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        binding.tvDontHaveAnAccountLogin.setOnClickListener {
+            goTOSignUp()
+        }
+    }
+    private fun goTOSignUp(){
+        Navigation.findNavController(requireView()).navigate(R.id.action_loginFragment_to_signupFragment)
     }
 }
--- app/src/main/java/com/ctwofinalproject/ticketing/view/ui/splash/SplashFragment.kt
+++ app/src/main/java/com/ctwofinalproject/ticketing/view/ui/splash/SplashFragment.kt
@@ -25,7 +25,7 @@ class SplashFragment : Fragment() {
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
         binding?.cvGetStarted?.setOnClickListener{
-            goToSignUp()
+            goToLogin()
         }
     }
     private fun goToLogin(){
