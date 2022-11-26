4469db6e7097d16d1791f2b0d58c472b4d9e1437
Roisull <lusiro2001@students.amikom.ac.id>
2022-11-27 01:50:53 +0700

    add vector asset and make navigation home

---
+++ app/src/main/java/com/ctwofinalproject/ticketing/view/MainActivityHome.kt
@@ -0,0 +1,12 @@
+package com.ctwofinalproject.ticketing.view
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.ctwofinalproject.ticketing.R
+
+class MainActivityHome : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main_home)
+    }
+}
