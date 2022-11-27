13d05b5f7926695588cd2823dd5007942063a575
zainzaidanahsan <ahsanzidan60@gmail.com>
2022-11-27 22:17:36 +0700

    bottom nav zain push

--- app/src/main/java/com/ctwofinalproject/ticketing/view/MainActivityHome.kt
+++ app/src/main/java/com/ctwofinalproject/ticketing/view/MainActivityHome.kt
@@ -2,11 +2,41 @@ package com.ctwofinalproject.ticketing.view
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.fragment.app.Fragment
+import androidx.fragment.app.replace
 import com.ctwofinalproject.ticketing.R
+import com.ctwofinalproject.ticketing.databinding.ActivityMainHomeBinding
+import com.ctwofinalproject.ticketing.view.ui.home.HomeFragment
+import com.ctwofinalproject.ticketing.view.ui.profile.ProfileFragment
 
 class MainActivityHome : AppCompatActivity() {
+    lateinit var binding : ActivityMainHomeBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main_home)
+        binding = ActivityMainHomeBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+        replaceFragment(HomeFragment())
+
+
+        binding.bottomNav.setOnItemReselectedListener{
+
+            when(it.itemId){
+
+                R.id.iconHomeBN -> replaceFragment(HomeFragment())
+                R.id.iconProfileBN -> replaceFragment(ProfileFragment())
+
+            else -> {
+
+                }
+            }
+            true
+        }
+    }
+
+    private fun replaceFragment(fragment: Fragment){
+        val fragmentManager = supportFragmentManager
+        val fragmentTransaction = fragmentManager.beginTransaction()
+        fragmentTransaction.replace(R.id.frameLayout, fragment)
+        fragmentTransaction.commit()
     }
 }
