60cffd62f239f265a864240aa5e23759beee94f0
Roisull <lusiro2001@students.amikom.ac.id>
2022-11-27 19:12:44 +0700

    make item_card_trip for recycler view

--- app/src/main/java/com/ctwofinalproject/ticketing/view/MainActivityHome.kt
+++ app/src/main/java/com/ctwofinalproject/ticketing/view/MainActivityHome.kt
@@ -3,10 +3,14 @@ package com.ctwofinalproject.ticketing.view
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import com.ctwofinalproject.ticketing.R
+import com.ctwofinalproject.ticketing.databinding.ActivityMainHomeBinding
 
 class MainActivityHome : AppCompatActivity() {
+    lateinit var binding: ActivityMainHomeBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main_home)
+        binding = ActivityMainHomeBinding.inflate(layoutInflater)
+        setContentView(binding.root)
     }
 }
