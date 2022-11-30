e624787e718f443625e771253ffb6fa10cc3de08
MSI\Irvan Wijaya <irfanwijayasardam@gmail.com>
2022-11-30 16:58:34 +0700

    Bottomnav fixed , now can change the layout when the bottomnav clicked

--- app/src/main/java/com/ctwofinalproject/ticketing/view/MainActivity.kt
+++ app/src/main/java/com/ctwofinalproject/ticketing/view/MainActivity.kt
@@ -1,30 +1,39 @@
 package com.ctwofinalproject.ticketing.view
 
+import android.content.ContentValues.TAG
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+import android.view.MenuItem
+import androidx.navigation.NavController
+import androidx.navigation.Navigation.findNavController
+import androidx.navigation.findNavController
+import androidx.navigation.fragment.NavHostFragment
+import androidx.navigation.ui.AppBarConfiguration
+import androidx.navigation.ui.onNavDestinationSelected
+import androidx.navigation.ui.setupActionBarWithNavController
+import androidx.navigation.ui.setupWithNavController
 import com.ctwofinalproject.ticketing.R
 import com.ctwofinalproject.ticketing.databinding.ActivityMainBinding
 import com.ctwofinalproject.ticketing.view.ui.home.HomeFragment
 import com.ctwofinalproject.ticketing.view.ui.profile.ProfileFragment
+import com.google.android.material.bottomnavigation.BottomNavigationView
 import dagger.hilt.android.AndroidEntryPoint
 
 @AndroidEntryPoint
 class MainActivity : AppCompatActivity() {
-    lateinit var binding: ActivityMainBinding
+    lateinit var binding                                                : ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
-        binding.bottomNav.setOnItemReselectedListener {
-            when(it.itemId){
+        val bottomNavigationView = findViewById<BottomNavigationView>(R.id.bottomNav)
+        val navHostFragment = supportFragmentManager.findFragmentById(R.id.fragmentContainerView) as NavHostFragment
+        val navController = navHostFragment.navController
 
+        bottomNavigationView.setupWithNavController(navController)
 
-                else -> {
-
-                }
-            }
-            true
-        }
     }
 }
--- app/src/main/java/com/ctwofinalproject/ticketing/view/ui/home/HomeFragment.kt
+++ app/src/main/java/com/ctwofinalproject/ticketing/view/ui/home/HomeFragment.kt
@@ -5,10 +5,14 @@ import android.os.Bundle
 import android.util.Log
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
+import android.view.MenuItem
 import android.view.View
 import android.view.ViewGroup
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.ViewModelProvider
+import androidx.navigation.NavController
+import androidx.navigation.fragment.NavHostFragment
+import androidx.navigation.ui.setupWithNavController
 import com.bumptech.glide.Glide
 import com.ctwofinalproject.ticketing.R
 import com.ctwofinalproject.ticketing.databinding.FragmentHomeBinding
@@ -18,10 +22,9 @@ import com.denzcoskun.imageslider.models.SlideModel
 import com.google.android.material.bottomnavigation.BottomNavigationView
 
 class HomeFragment : Fragment() {
-    private var _binding : FragmentHomeBinding?             = null
-    private val binding get()                               = _binding!!
-    lateinit var viewModelProto                             : ProtoViewModel
-
+    private var _binding : FragmentHomeBinding?                         = null
+    private val binding get()                                           = _binding!!
+    lateinit var viewModelProto                                         : ProtoViewModel
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
@@ -37,7 +40,6 @@ class HomeFragment : Fragment() {
         setImageSlider()
         setProfile()
         setBottomNav()
-
 
         viewModelProto.dataUser.observe(viewLifecycleOwner, {
             Log.d(TAG, "onViewCreated: ${it}")
